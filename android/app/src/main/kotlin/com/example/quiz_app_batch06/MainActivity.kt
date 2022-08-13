b3df72910502ab5d6d74bcf1b215c6c6424ba5e0
Rassel98 <mdjahidulahsan890@gmail.com>
2022-08-13 18:03:37 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/quiz_app_batch06/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quiz_app_batch06
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/customwidgets/question_set_widget.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:quiz_app_batch06/question_set.dart';
+
+class QuestionSetView extends StatefulWidget {
+  final int index;
+  final QuestionSet question;
+  final Function(String) onAnswered;
+  const QuestionSetView(
+      {Key? key, required this.index,
+        required this.question,
+        required this.onAnswered}) : super(key: key);
+
+  @override
+  State<QuestionSetView> createState() => _QuestionSetViewState();
+}
+
+class _QuestionSetViewState extends State<QuestionSetView> {
+  String groupValue = '';
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Text('${widget.index + 1}.'),
+          title: Text(widget.question.question),
+          subtitle: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: widget.question.options.map((e) => ListTile(
+              leading: Radio<String>(
+                value: e,
+                groupValue: groupValue,
+                onChanged: (value) {
+                  setState(() {
+                    groupValue = value as String;
+                  });
+                  widget.onAnswered(groupValue);
+                },
+              ),
+              title: Text(e),
+            )).toList(),
+          ),
+        )
+      ],
+    );
+  }
+}
